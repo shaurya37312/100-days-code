1845764f927b35e39a78775c377b47b3a853c62b
shaurya37312 <Shaurya.37312@stu.upes.ac.in>
2026-09-24 20:29:09 +0530

    Add program to check if integer is positive, negative, or zero

---
+++ 100_days_program/D6_Q2.c
@@ -0,0 +1,21 @@
+// Write a program to input an integer and check whether it is positive, negative or zero using nested if–else.
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (n >= 0) {
+        if (n == 0) {
+            printf("The number is zero.\n");
+        } else {
+            printf("The number is positive.\n");
+        }
+    } else {
+        printf("The number is negative.\n");
+    }
+
+    return 0;
+}
